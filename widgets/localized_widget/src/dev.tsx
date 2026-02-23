9dfebddbf6cb707ccb7cc98a9aa82831b7723043
Łukasz Wnęk <lukasz.wnek@gainsight.com>
2026-02-23 17:44:02 +0100

    Fix unused import warning in dev.tsx
    GE-241866

--- widgets/localized_widget/src/dev.tsx
+++ widgets/localized_widget/src/dev.tsx
@@ -1,8 +1,7 @@
 import { init } from "./main";
-import type { WidgetSDK } from "./types";
 
 init({
   whenReady: () => Promise.resolve(),
   shadowRoot: document.getElementById("widget-root")! as unknown as ShadowRoot,
   on: () => () => {},
-});
+} satisfies import("./types").WidgetSDK);
