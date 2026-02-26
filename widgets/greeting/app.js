85c51adcc854124632867d7c39ca2edd2e0232e6
Łukasz Wnęk <lukasz.wnek@gainsight.com>
2026-02-26 10:55:26 +0100

    debug(greeting): add console logs for rendering troubleshooting
    GE-241941

--- widgets/greeting/app.js
+++ widgets/greeting/app.js
@@ -1,13 +1,24 @@
 export async function init(sdk) {
+  console.log('[greeting] init called', sdk);
   await sdk.whenReady();
+  console.log('[greeting] sdk ready');
   const render = (props) => {
+    console.log('[greeting] render called with props', props);
     sdk.$('.card').style.background = props.color;
     sdk.$('#title').textContent = props.title;
     sdk.$('#message').textContent = props.message;
+    console.log('[greeting] render complete', {
+      card: sdk.$('.card'),
+      title: sdk.$('#title'),
+      message: sdk.$('#message'),
+    });
   };
-  render(sdk.getProps());
+  const props = sdk.getProps();
+  console.log('[greeting] initial props', props);
+  render(props);
   sdk.on('propsChanged', render);
   sdk.on('destroy', () => {
+    console.log('[greeting] destroy called');
     sdk.$('.card').innerHTML = '';
   });
 }
