f2f72e33501522e674cac66fd2dbf48e883eb32b
Łukasz Wnęk <lukasz.wnek@gainsight.com>
2026-03-09 18:51:13 +0100

    feat: add styled_widget with styled-components
    Simple counter widget on port 5174 for testing yarn preview with
    multiple widgets.
    
    GE-242583-4

---
+++ widgets/styled_widget/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig(({ mode }) => {
+  const { VITE_TUNNEL_URL } = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      react(),
+      // TODO: remove this plugin and VITE_TUNNEL_URL once widget-service rewrites asset paths using content.endpoint origin instead of the CC domain
+      {
+        name: "absolute-tunnel-urls",
+        transformIndexHtml: (html) =>
+          VITE_TUNNEL_URL
+            ? html.replace(/src="\/(.*?)"/g, `src="${VITE_TUNNEL_URL}/$1"`)
+            : html,
+      },
+    ],
+    server: {
+      allowedHosts: true,
+      cors: true,
+      hmr: false,
+    },
+  };
+});
